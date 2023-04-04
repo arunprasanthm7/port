4fa76eb53a134b0ae7ea13361329881596789150
Arunprasanthm7 <arunprasanthm7@gmail.com>
2023-04-04 19:35:11 +0530

    design changes updated

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 const mode = document.getElementById("mode");
 mode.addEventListener("click", () => {
-  const body = document.getElementsByTagName("BODY")[0];
-  const r = document.querySelector(":root");
-  const modeName = document.getElementById("modeName");
-  const onOffBtn = document.getElementById("onOffBtn");
-  if (modeName.innerText == "Day") {
-    modeName.innerText = "";
-    onOffBtn.style.width = "60px";
-    onOffBtn.innerText = "Night";
-    r.style.setProperty("--white", "rgb(35, 33, 33)");
-    r.style.setProperty("--black", "white");
-    r.style.setProperty("--gridBoxShd", " 1px 1px 8px 8px  rgb(120, 120, 120)");
-    r.style.setProperty("--borderBot", " 1px solid  rgb(0, 0, 0)");
-    r.style.setProperty("--bg", "rgb(19, 17, 17)");
-    
-
-  } else {
-    onOffBtn.innerText = "";
-    onOffBtn.style.width = "24px";
-    modeName.innerText = "Day";
-    r.style.setProperty("--white", "white");
-    r.style.setProperty("--gridBoxShd", "1px 1px 8px 8px rgb(215, 213, 213)");
-    r.style.setProperty("--borderBot", " 1px solid  rgb(218, 217, 217)");
-    r.style.setProperty("--lightGrey", "rgb(185, 185, 185)");
-    r.style.setProperty("--black", "rgb(19, 17, 17)");
-    r.style.setProperty("--bg", "rgb(231, 231, 231)");
-   
-  }
+  // let body = document.getElementsByTagName("BODY")[0];
+  let light = document.getElementById('light');
+  let lightIcon = document.getElementById('lightIcon');
+  document.body.classList.toggle('dark-theme')
+if(document.body.classList.contains('dark-theme')){
+     light.style.transform = 'translateX(36px)';
+     lightIcon.src = 'assets/icons8-moon-symbol-64.png';
+}
+ else{
+  light.style.transform = 'translateX(0px)';
+  lightIcon.src = 'assets/icons8-sun.svg';
+ }
 });
 let showSlides = (n) => {
   let slides = document.getElementsByClassName("slide");
