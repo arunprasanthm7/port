edc5888fe31a600400e66ee2f390a514b15b2537
Arunprasanthm7 <arunprasanthm7@gmail.com>
2023-04-06 10:29:08 +0530

    page flow is updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mode = document.getElementById("mode");
 mode.addEventListener("click", () => {
-  // let body = document.getElementsByTagName("BODY")[0];
   let light = document.getElementById('light');
   let lightIcon = document.getElementById('lightIcon');
   document.body.classList.toggle('dark-theme')
@@ -51,3 +50,79 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
+  //navElements
+  let about=document.getElementById('about');
+  let home=document.getElementById('home');
+  let projects=document.getElementById('projects');
+  //cardElements
+  let bio=document.getElementById('bio')
+  let slider=document.getElementById('slider');
+  let skill= document.getElementById('skill');
+  let tool= document.getElementById('tool');
+  let techtips= document.getElementById('techtips');
+  let githubView= document.getElementById('githubView');
+  let interaction= document.getElementById('interaction');
+  let project1= document.getElementById('project1');
+  let project2= document.getElementById('project2');
+  let instagram= document.getElementById('instagram');
+  let twitter= document.getElementById('twitter');
+  let gitHub= document.getElementById('gitHub');
+  let linkedIn=document.getElementById('linkedIn');
+  about.addEventListener("click",()=>{
+    about.classList.add('nav-active');
+    home.classList.remove('nav-active');
+    projects.classList.remove('nav-active');
+    bio.style.gridArea='1/1/1/3';
+    slider.style.gridArea='1/3/1/3';
+    skill.style.gridArea='4/4/4/4';
+    linkedIn.style.gridArea='1/4/1/4';
+    tool.style.gridArea='3/4/3/4';
+    techtips.style.gridArea='5/4/5/4';
+    githubView.style.gridArea='5/2/5/4';
+    interaction.style.gridArea='3/1/5/1';
+    instagram.style.gridArea='2/4/2/4';
+    twitter.style.gridArea='2/3/2/3';
+    gitHub.style.gridArea='2/1/2/3';
+    project1.style.gridArea='3/2/5/2';
+    project2.style.gridArea='3/3/5/3';
+    interaction.style.opacity='0.2';
+    project1.style.opacity='0.2';
+    project2.style.opacity='0.2';
+    githubView.style.opacity='0.2';
+    techtips.style.opacity='0.2';
+    tool.style.opacity='0.2';
+    skill.style.opacity='0.2';
+    linkedIn.style.opacity='1';
+    instagram.style.opacity='1';
+    slider.style.opacity='1';
+  })
+
+  projects.addEventListener('click',()=>{
+    about.classList.remove('nav-active');
+    home.classList.remove('nav-active');
+    projects.classList.add('nav-active');
+    bio.style.gridArea='1/3/1/5';
+    slider.style.gridArea='4/2/4/2';
+    skill.style.gridArea='2/3/2/3';
+    tool.style.gridArea='2/4/2/4';
+    techtips.style.gridArea='4/4/4/4';
+    githubView.style.gridArea='5/2/5/4';
+    interaction.style.gridArea='4/1/6/1';
+    project1.style.gridArea='1/1/3/1';
+    project2.style.gridArea='1/2/3/2';
+    instagram.style.gridArea='5/4/5/4';
+    twitter.style.gridArea='3/3/3/5';
+    linkedIn.style.gridArea='4/3/4/3';
+    gitHub.style.gridArea='3/1/3/3';
+    interaction.style.opacity='0.2';
+    slider.style.opacity='0.2';
+    githubView.style.opacity='0.2';
+    techtips.style.opacity='0.2';
+    linkedIn.style.opacity='0.2';
+    instagram.style.opacity='0.2';
+    tool.style.opacity='1';
+    skill.style.opacity='1';
+    project1.style.opacity='1';
+    project2.style.opacity='1';
+  })
+  media
