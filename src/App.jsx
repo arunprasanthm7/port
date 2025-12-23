2c3c381f7f93541069a9f61e4621dddb15d2ba84
arunprasanthm7 <arunprasanthm7@gmail.com>
2025-12-23 21:28:08 +0530

    feat: add exchange icon for version toggle and improve scroll-to-top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,6 @@ function App() {
       <div ref={contactRef}>
         <ContactForm />
       </div>
-
       <svg className="absolute inset-0 w-full h-full">
         <defs>
           <linearGradient id="Gradient" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -99,6 +98,20 @@ function App() {
             />
           )}
         </div>
+        <div
+          className={`transition-all duration-300 ${
+            scrolled ? "bg-slate-500 h-5 mx-2 w-[1px]" : "h-0 w-0 mx-0"
+          }`}
+        ></div>
+        <RiExchangeFundsLine
+          title="Version Toggle"
+          onClick={() => window.scrollTo({ top: "0", behavior: "smooth" })}
+          className={`transition-all duration-300 ${
+            scrolled
+              ? "h-8 w-8 text-white cursor-pointer hover:fill-[url(#Gradient)] duration-300 hover:scale-110 sm:fill-[url(#Gradient)] "
+              : "h-0 w-0"
+          }`}
+        />
         <div
           className={`transition-all duration-300 ${
             scrolled ? "bg-slate-500 h-5 mx-2 w-[1px]" : "h-0 w-0 mx-0"
